70f40e3e66867690c7391630a3fc48f3b1a7fb5d
Nicolás Pereyra <ednip7@gmail.com>
2021-12-20 16:36:04 -0300

    Add Launch arguments to enable/disable visualization of rays

--- irobot_create_common_bringup/launch/robot_description.launch.py
+++ irobot_create_common_bringup/launch/robot_description.launch.py
@@ -5,14 +5,20 @@
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.substitutions import Command, PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, PathJoinSubstitution, LaunchConfiguration
 from launch_ros.actions import Node
 
+ARGUMENT = [
+ DeclareLaunchArgument('visualize_rays', default_value='false',
+                          choices=['true', 'false'],
+                          description='Enable/disable ray visualization'),
+]
 
 def generate_launch_description():
     pkg_create3_description = get_package_share_directory('irobot_create_description')
     xacro_file = PathJoinSubstitution([pkg_create3_description, 'urdf', 'create3.urdf.xacro'])
-
+    visualize_rays = LaunchConfiguration('visualize_rays')
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -20,7 +26,8 @@ def generate_launch_description():
         output='screen',
         parameters=[
             {'use_sim_time': True},
-            {'robot_description': Command(['xacro', ' ', xacro_file])},
+            {'robot_description': Command(
+                ['xacro', ' ', xacro_file, ' ', 'visualize_rays:=', visualize_rays])},
         ],
     )
 
@@ -32,7 +39,7 @@ def generate_launch_description():
     )
 
     # Define LaunchDescription variable
-    ld = LaunchDescription()
+    ld = LaunchDescription(ARGUMENT)
     # Add nodes to LaunchDescription
     ld.add_action(joint_state_publisher)
     ld.add_action(robot_state_publisher)
